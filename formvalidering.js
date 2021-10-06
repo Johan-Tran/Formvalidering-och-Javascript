7a009162e8453d6dd5a614bf7460e775115c2299
Johan-Tran <johan.tran@elev.ga.ntig.se>
2021-10-06 12:03:05 +0200

    Update formvalidering.js

--- formvalidering.js
+++ formvalidering.js
@@ -1,16 +1,23 @@
 const max = 10;
+//Använder objektet array för att spara flera värden. 
 var rndNums = [],
   range = 100;
 
+//Lägger till ett antal element i fältet.
 function Arrlength(max, rndNums) {
+  //Har använt for-loop för att man kan specifiera antalet element man vill ha i fältet.
   for (let i = 0; i < max; i++) {
     rndNums.push(i);
   }
   return rndNums;
 }
 
+//Ersätter element vid ett specifikt index med ett slumpmässigt tal. 
 function RandomizeNums(rndnums, range) {
+  //Behöver använda for-loop för att ta reda på indexen och ersätta elementet.
   for (let i = 0; i < rndnums.length; i++) {
+    //Använder objekten Math för att avrunda neråt till det närmaste talet och slumpmässigt få fram ett tal 
+    //(Math.floor avrundar och Math.random ger ett slumpmässigt tal).
     let rnd = Math.floor(Math.random() * range) + 1;
     if (!rndNums.includes(rnd)) {
       rndnums[i] = rnd;
@@ -19,12 +26,19 @@ function RandomizeNums(rndnums, range) {
   return rndNums;
 }
 
-
+//Använder objektet document för att få värden på rubrikerna och det man har skrivit in i formen.
 document.getElementById("Arr").innerHTML = RandomizeNums(Arrlength(max, rndNums), range);
+//Sort metoden jämför två parametrar returnerar antingen något som är mindre än 0, 0 eller större än 0. 
+/*
+Om a < b returneras ett tal mindre 0, som då sorterar a före b.
+Om a > b returneras ett tal mindre 0, som då sorterar b före a.
+Om a == b returneras 0, vilket betyder att parameterna index inte ändras.
+*/ 
 document.getElementById("Arr2").innerHTML = rndNums.sort(function (a, b) {
   return a - b;
 });
 
+//Kollar om man har skrivit ner allting och checkat "Terms of Service".
 function VerifyForm() {
   let emailStatus, passwordStatus, userStatus;
   let checkBoxStatus = document.getElementById("check").checked;
@@ -36,15 +50,20 @@ function VerifyForm() {
   VerifyAll(emailStatus, passwordStatus, userStatus, checkBoxStatus);
 }
 
+//Funktion för att öppna en ny sida.
 function NewPage() {
   window.open("confirmed.html", "_blank");
 }
 
+//Funktion som kollar om man har skrivit en email.
 function VerifyEmail(emailStatus) {
+  //Ett fält som har flera email-tjänster.
   const mail = ["@gmail.se", "@gmail.com", "@hotmail.se", "@hotmail.com", "@yahoo.se", "@yahoo.com", "@outlook.se", "@outlook.com"];
   let email = document.getElementById("email").value;
 
+  //Använder mig av en for-loop för att kolla om någon av email-tjänsterna stämmer med det man har skrivit i email.
   for (let i = 0; i < mail.length; i++) {
+    
     let emailtmp = email.slice(0, -mail[i].length);
     if (email.includes(mail[i], (emailtmp.length))) {
       emailStatus = true;
@@ -54,15 +73,17 @@ function VerifyEmail(emailStatus) {
   return emailStatus;
 }
 
+//Funktionen kollar om lösenorden stämmer.
 function VerifyPassword(password, passwordConfirm) {
   password = document.getElementById("password").value;
   passwordConfirm = document.getElementById("passwordConfirm").value;
   return password === passwordConfirm && password.length >= 6;
 }
 
+//En funktion som kollar om username har mer än 6 karaktärer.
 function VerifyUser() {
   let username = document.getElementById("username").value;
-  return username.length >= 6 && username !== "";
+  return username.length >= 6;
 }
 
 function VerifyAll(emailStatus, passwordStatus, userStatus, checkBoxStatus) {
